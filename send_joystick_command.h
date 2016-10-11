75320eba8a79f9b27e3ef118a07d2c21147dfc8b
Philippe Crochat <pcrochat42@gmail.com>
2016-10-11 18:42:48 +0200

    Delete send_joystick_command.h

--- send_joystick_command.h
+++
@@ -1,8 +0,0 @@
-#define JOY_DEV "/dev/input/js0"
-#define NAME_LENGTH 128
-#define KEEPALIVE 200 //in milliseconds, when there is more than KEEPALIVE ms elapsed we resend the packet even if it is the same
-#define PACKET_LENGTH 26
-#define MAX_RATE 2000 //max 1 packet every MAX_RATE microseconds
-
-//reattribute those channels based on your needs
-int CHANNEL_INDEX[8]={1, 4, 3, 0, 2, 5, 6, 7};
